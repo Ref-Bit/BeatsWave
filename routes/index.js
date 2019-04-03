08bf16b8308c65d2ee75de0194458aa75bbb1970
Ref-Bit <[rsv.gho@gmail.com]>
2019-04-03 17:46:12 +0300

    The 404 page is added and the navbar links are fixed.

--- routes/index.js
+++ routes/index.js
@@ -11,5 +11,9 @@ router.get('/contact', function(req, res, next) {
   res.render('contact', { title: 'BeatsWave - Contact Us' });
 });
 
+/* GET 404 page. */
+router.get('/*', function (req, res) {
+  res.render('404', { title: 'BeatsWave - 404' });
+});
 
 module.exports = router;
